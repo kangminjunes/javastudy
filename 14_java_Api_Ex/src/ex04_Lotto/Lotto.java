16529598de854a819c5b9b4cf821e9b5f4db92e1
kangminjunes <1999koco@naver.com>
2023-08-02 16:05:03 +0900

    20230802

--- 14_java_Api_Ex/src/ex04_Lotto/Lotto.java
+++ 14_java_Api_Ex/src/ex04_Lotto/Lotto.java
@@ -1,5 +1,26 @@
 package ex04_Lotto;
 
 public class Lotto {
-
+  
+  /**
+   * 로또 구매<br>
+   * 로또 구매를 위해서 사용자로부터 money를 입력 받는 메소드<br>
+   * 로또 구매 비용은 최소 1000, 최대 100000이다.<br>
+   * @return money 사용자가 입력한 로또 구매 비용에서 1000 미만 단위는 버리고 반환. ex) 5999 입력 시 5000 반환
+   * @return 0 로또 구매 실패 시 반환
+   */
+  public int buyLotto() {
+    
+  }
+  
+  /**
+   * 로또 번호 생성<br>
+   * 로또 구매 비용만큼 로또 번호를 생성하는 메소드<br>
+   * 5게임씩 끊어서 생성된 로또 번호를 출력한다.<br>
+   * @param money 로또 구매 비용
+   */
+  public void generateLotto(int money) {
+    
+  }
+  
 }
--- 14_java_Api_Ex/src/ex04_Lotto/LottoMain.java
+++ 14_java_Api_Ex/src/ex04_Lotto/LottoMain.java
@@ -2,4 +2,11 @@ package ex04_Lotto;
 
 public class LottoMain {
 
+  public static void main(String[] args) {
+    
+    Lotto lotto = new Lotto();
+    lotto.generateLotto(lotto.buyLotto());
+
+  }
+
 }
