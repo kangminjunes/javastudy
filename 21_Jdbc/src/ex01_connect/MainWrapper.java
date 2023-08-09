456bf18f69d019e574a974c3c800e0afbf149c6b
kangminjunes <1999koco@naver.com>
2023-08-09 09:53:16 +0900

    20230809

--- 21_Jdbc/src/ex01_connect/MainWrapper.java
+++ 21_Jdbc/src/ex01_connect/MainWrapper.java
@@ -10,9 +10,7 @@ public class MainWrapper {
   public static void ex01() {
     
     try {
-      
       Class.forName("oracle.jdbc.OracleDriver");
-      
     } catch (ClassNotFoundException e) {
       System.out.println("OracleDriver 클래스가 없다.");
     }
@@ -23,7 +21,7 @@ public class MainWrapper {
   public static void ex02() {
     
     // 접속 정보
-    String url = "jdbc:oracle:thin:localhost:1521:xe";
+    String url = "jdbc:oracle:thin:@localhost:1521:xe";
     String user = "GD";
     String password = "1111";
     
@@ -37,18 +35,28 @@ public class MainWrapper {
     } catch (SQLException e) {
       System.out.println("DB 접속 실패");
     } finally {
-    // Connection 객체 소멸
-    try {
-      if(con != null) con.close();
-    } catch (SQLException e) {
-      System.out.println("DB 접속 닫기 실패");
+      // Connection 객체 닫기
+      try {
+        if(con != null) con.close();
+      } catch (SQLException e) {
+        System.out.println("DB 접속 닫기 실패");
+      }
     }
-   }
- }
+    
+  }
+  
+  // 3. 접속 정보를 별도의 파일(프로퍼티 파일)에 저장 (.gitignore 대비)
+  
   
   public static void main(String[] args) {
-    
     ex01();
     ex02();
   }
+
 }
+
+
+
+
+
+
